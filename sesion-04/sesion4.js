82931b8197221575b07bdb5bb742140a56fe5dba
JCCoronaGN <jc.corona.cit@gmail.com>
2022-10-13 14:13:38 -0500

    Sesion 4 Arreglos y Objetos

--- sesion-04/sesion4.js
+++ sesion-04/sesion4.js
@@ -81,5 +81,135 @@ console.log(tarea.status);
 tarea.saluda();
 console.log(tarea.nombres);
 
+var color1 = "Red";
+var color2 = "Blue";
+var color3 = "Green";
+console.log(color1,color2,color3);
+
+var colors = [color1,color2,color3];
+console.log("colors : ",colors);
+color1 = "Magenta";
+console.log("colors : ",colors);
+var colors = ["Red","Blue","Green"];
+
+colors[0] = "Yellow";
+console.log("colors : ",colors);
+console.log("Color 0 : ",colors[0]);
+console.log("Color 4 : ",colors[4]);
+
+colors.push("Brown");
+console.log("colors : ",colors);
+
+var cadena = "Hola";
+console.log(cadena[0]);
 
 
+function multiplyByTwo(numbersArray) {
+    var multipledNumbers = [];
+    for (let index = 0; index < numbersArray.length; index++) {
+        multipledNumbers.push(numbersArray[index]*2);
+    }
+    return multipledNumbers;
+}
+console.log("Con funcion ",multiplyByTwo([1,2,3]));
+
+console.log("Con funcion flecha ",[1,2,3].map(x=>x*2));
+
+console.log("Con anonima",[1,2,3].map(function(x){return x*2}));
+
+ages=[23,45,31,16,29];
+function calculateAverage(agesArray){
+    var sumaEdad = 0;
+    for (let index = 0; index < agesArray.length; index++) {
+        sumaEdad += agesArray[index];
+    }
+    return sumaEdad/agesArray.length;
+}
+
+console.log("Con funcion",calculateAverage(ages));
+
+var calculateAverageAnon = function (agesArray){
+    var sumaEdad = 0;
+    for (let index = 0; index < agesArray.length; index++) {
+        sumaEdad += agesArray[index];
+    }
+    return sumaEdad/agesArray.length;
+}
+
+console.log("Con funciona anonima",calculateAverageAnon(ages));
+
+var calculateAverageAnon2 = (agesArray)=>{
+    var sumaEdad = 0;
+    for (let index = 0; index < agesArray.length; index++) {
+        sumaEdad += agesArray[index];
+    }
+    return sumaEdad/agesArray.length;
+};
+console.log("Con funcion arrow",calculateAverageAnon2(ages));
+
+// Objetos
+
+var john = {
+    firstName: "Juan",
+    lastName: "Crown",
+    birthYear: 1971,
+    favoriteColors: ["Red","Green","Blue"],
+    calculateAge: function(birthYear){
+        var today = new Date();
+        var year = today.getFullYear();
+        return year - birthYear;
+    },
+    calculateAge2: function(){
+        var today = new Date();
+        var year = today.getFullYear();
+        return year - this.birthYear;
+    }
+}
+
+var functionArray = [function(){
+    console.log("hola mundo, fuinciona desde un arreglo");
+}]
+functionArray[0];
+functionArray[0]();
+
+
+console.log(john);
+console.log(john.lastName);
+console.log(john["firstName"]);
+
+console.log(john.calculateAge(john.birthYear));
+console.log(john.calculateAge2());
+
+// Ejemplo - convertir objeto a arreglo
+
+var car ={
+    "brand": "Nissan",
+    model: "Versa",
+    year:  2020
+};
+
+console.log(Object.keys(car));
+
+function keyValuesPairs(obj) {
+    var keys = Object.keys(obj);
+    var pairs = [];
+    for (let i = 0; i < keys.length; i++) {
+        pairs.push([keys[i],obj[keys[i]]]);        
+    }
+    return pairs;
+}
+console.log(keyValuesPairs(car));
+
+var people = [
+    john,john
+]
+
+ console.log(people);
+
+ var people = [
+    john,{
+        firstName: "Maria",
+        lastName: "Doe"
+    }
+]
+console.log(people);
